3ba9567bbb6a044040b9f62a0dbcdaf081830621
SidakbirSingh <73062815+SidakbirSingh@users.noreply.github.com>
2021-01-24 23:00:08 +0530

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,20 @@
+class Food{
+
+constructor(){
+    var foodStock
+    var lastFed
+    var MilkBottle
+}
+
+MilkBottle=loadImage("images/Milk.png")
+getFoodStock(){
+
+}
+updateFoodStock(){
+
+}
+deductFood(){
+
+
+}
+}
